de4018a6c8f22a7db0403a87fc82938bd7fb6d52
Elisabeth <elireyesgarcia8@gmail.com>
2024-04-07 15:43:03 -0400

    Breaking code is my second favorite activity in this world

--- Assets/Scripts/pDrawCards.cs
+++ Assets/Scripts/pDrawCards.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawCards : MonoBehaviour
 {
@@ -34,16 +36,136 @@ public class DrawCards : MonoBehaviour
     public GameObject Card27;
 
     public GameObject PlayerHand;
-    
-    public  List<GameObject> pDeck = new List<GameObject>();
 
-    public void OnClick(){
-        for(int i = 0; i < 10; i ++){
-            GameObject card = Instantiate(pDeck[Random.Range(0, pDeck.Count)], new Vector2(0,0), Quaternion.identity);
-            card.transform.SetParent(PlayerHand.transform, false);
-            pDeck.RemoveAt(0);
+    public GameObject[] cards; // Array to hold references to card GameObjects
+    public Text winnerText; // Reference to the UI Text component for displaying winner information
+
+    private List<GameObject> pDeck = new List<GameObject>(); // List to hold references to all card GameObjects in the deck
+    private bool[] Dealt; // Array to track whether each card has been dealt
+    private int round = 1; // Variable to track the current round
+
+    void Start()
+    {
+        Dealt = new bool[cards.Length]; // Initialize the dealt array
+
+        // Fill the deck with references to all card GameObjects
+        foreach (GameObject card in cards)
+        {
+           pDeck.Add(Card1);
+           pDeck.Add(Card2);
+           pDeck.Add(Card3);
+           pDeck.Add(Card4);
+           pDeck.Add(Card5);
+           pDeck.Add(Card6);
+           pDeck.Add(Card7);
+           pDeck.Add(Card8);
+           pDeck.Add(Card9);
+           pDeck.Add(Card10);
+           pDeck.Add(Card11);
+           pDeck.Add(Card12);
+           pDeck.Add(Card13);
+           pDeck.Add(Card14);
+           pDeck.Add(Card15);
+           pDeck.Add(Card16);
+           pDeck.Add(Card17);
+           pDeck.Add(Card18);
+           pDeck.Add(Card19);
+           pDeck.Add(Card20);
+           pDeck.Add(Card21);
+           pDeck.Add(Card22);
+           pDeck.Add(Card23);
+           pDeck.Add(Card24);
+           pDeck.Add(Card25);
+           pDeck.Add(Card26);
+           pDeck.Add(Card27);
+        }
+
+        // Shuffle the deck
+        ShuffleDeck();
+    }
+
+    void ShuffleDeck()
+    {
+        // Fisher-Yates shuffle algorithm to shuffle the deck
+        for (int i = pDeck.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            GameObject temp = pDeck[i];
+            pDeck[i] = pDeck[j];
+            pDeck[j] = temp;
         }
     }
+
+    public void DealCards( int NumOfCards = 10)
+    {
+        // Deal the specified number of cards to the player's hand
+        for (int i = 0; i < NumOfCards; i++)
+        {
+            // Find the first undealt card in the deck
+            int index = FindUndealtCard();
+
+            if (index != -1)
+            {
+                // Instantiate the card and add it to the player's hand
+                GameObject card = Instantiate(pDeck[index], new UnityEngine.Vector2(0,0), UnityEngine.Quaternion.identity);
+                card.transform.SetParent(PlayerHand.transform, false);
+
+                // Mark the card as dealt
+                Dealt[index] = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    int FindUndealtCard()
+    {
+        // Find the index of the first undealt card in the deck
+        for (int i = 0; i < pDeck.Count; i++)
+        {
+            if (!Dealt[i])
+            {
+                return i;
+            }
+        }
+        return -1; // Return -1 if all cards have been dealt
+    }
+
+    public void OnClick()
+    {
+        // Deal cards based on the current round
+        switch (round)
+        {
+            case 1:
+                DealCards(10);
+                break;
+            case 2:
+                DealCards(3);
+                break;
+            case 3:
+                if (winnerText.text == "")
+                {
+                    DealCards(4);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Update()
+    {
+        // Update the current round from the GameManager or any other source
+        // For demonstration purposes, let's assume it's updated externally
+        // For example:
+        // round = GameManager.Instance.GetCurrentRound();
+    }
+}
+    
+   /* public  List<GameObject> pDeck = new List<GameObject>();
+
     void Start()
     {
        pDeck.Add(Card1);
@@ -74,22 +196,10 @@ public class DrawCards : MonoBehaviour
        pDeck.Add(Card26);
        pDeck.Add(Card27);
 
-        Shuffle(pDeck);
+       
     }
     
-    public static List <GameObject> Shuffle(List<GameObject> deck)
-    {
-        List<GameObject> pDeck = new List<GameObject>();
-
-        foreach(GameObject card in deck)
-        {
-            int position = Random.Range(0, deck.Count);
-            pDeck.Add((position == pDeck.Count) ? card : deck[position]);
-        }
-        return pDeck;
-    }  
     void Update()
     {
         
-    }
-}
+    }*/
