85c39f29331ba6a9b8829affd9d3750067ecbc75
Elisabeth <elireyesgarcia8@gmail.com>
2024-04-14 17:02:37 -0400

    Did something at the fringe class but aint worth it

--- Assets/Scripts/Classes/Fringe Class.cs
+++ Assets/Scripts/Classes/Fringe Class.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FringeClass : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject CardEntry;
+    public List<GameObject> CardsinFrange;
+    public int Cards = 0;
+    public int Sum = 0;
 
-    // Update is called once per frame
-    void Update()
+    // public Text puntuationText;
+    public string Faction;
+    public int Fringe;
+    public GameObject ShurimaGrave;
+    public GameObject ShadowIslesGrave;
+    public GameObject PlayerHand;
+    public GameObject EnemyHand;
+    public GameObject MeleeFringe;
+
+    private int CheckRound = 1;
+    private int Round = 1;
+    private int sumaparcial = 0;
+
+     private void OnCollisionEnter2D(Collision2D collision) //cuando colisionan mete las cartas en la lista de la franja 
     {
-        
+        CardEntry = collision.gameObject;
+        CardsinFrange.Add(CardEntry);
+        Cards ++;
     }
 }
