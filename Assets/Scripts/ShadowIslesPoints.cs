eb7a8908125eb013b08080fbbc8b66dddb632e32
Elisabeth <elireyesgarcia8@gmail.com>
2024-04-14 16:47:34 -0400

    Created the points scripts and the strip-fringe script, shit is going so wild rn im getting lost

--- Assets/Scripts/ShadowIslesPoints.cs
+++ Assets/Scripts/ShadowIslesPoints.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShadowIslesPoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text ShadowIslesText;
 
-    // Update is called once per frame
+    public GameObject EMP; //Enemy Melee Points
+    public GameObject ERP; //Enemy Range(Distance) Points
+    public GameObject EAP; //Enemy Siege(Asedio) Points 
+
+    private int ShadowIslesSum = 0;
+
     void Update()
     {
-        
+        int ShadowIslesContainer = EMP.GetComponent<FringeClass>().Sum + ERP.GetComponent<FringeClass>().Sum + EAP.GetComponent<FringeClass>().Sum;
+        ShadowIslesSum = ShadowIslesContainer;
+        ShadowIslesText.text = ShadowIslesSum.ToString();
     }
 }
--- Assets/Scripts/ShurimaPoints.cs
+++ Assets/Scripts/ShurimaPoints.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ShurimaPoints : MonoBehaviour
 {
-    
+    public Text ShurimaText;
+
+    public GameObject PMP; //Player Melee Points
+    public GameObject PRP; //Player Range(Distance) Points
+    public GameObject PAP; //Player Siege(Asedio) Points 
+
+    private int ShurimaSum = 0;
+
+    void Update()
+    {
+        int ShurimaSumContainer = PMP.GetComponent<FringeClass>().Sum + PRP.GetComponent<FringeClass>().Sum + PAP.GetComponent<FringeClass>().Sum;
+        ShurimaSum = ShurimaSumContainer;
+        ShurimaText.text = ShurimaSum.ToString();
+    }
 }
