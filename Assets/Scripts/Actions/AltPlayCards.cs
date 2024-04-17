bfde0981674a6d703675f6cf33fc3cf561921f1f
Elisabeth <elireyesgarcia8@gmail.com>
2024-04-17 01:10:48 -0400

    Yeah, now its working but its also freezing on the second play, define failure without saying my name (impossible)

--- Assets/Scripts/Actions/AltPlayCards.cs
+++ Assets/Scripts/Actions/AltPlayCards.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.RestService;
 using UnityEngine;
 
 public class AltPlayCards : MonoBehaviour
@@ -8,7 +9,7 @@ public class AltPlayCards : MonoBehaviour
    public bool Turn = true; // i dont have turns yet but yk
    public bool playable = true; //bool to avoid playing the same card mult times
    public bool GameOver;
-
+   private bool playedCards;
    public bool EnemySurrender;
    public bool PlayerSurrender;
    
@@ -31,17 +32,18 @@ public class AltPlayCards : MonoBehaviour
    //private List<GameObject> pDeck;
    //private List<GameObject> eDeck;
    // private int position= 0;
-
-   private bool PlayerChooses; //los jugadores eligieron sus cartas
-    private bool EnemyChooses;
    // private int CompPlayerCards = 0; //para comprobar las cartas de cada jugador
    // private int CompEnemyCards = 0;
+
+   private bool PlayerChooses; //los jugadores eligieron sus cartas
+   private bool EnemyChooses;
    private GameObject ShurimaHand; //player
    private GameObject ShadowIslesHand;  //enemy
+
     public void PlayCardsMethod()
 {
-    if (!GameOver)
-    {
+        if (!GameOver)
+        {
         PlayCardToZone(Card, PFMelee, 1, "Shurima");
         PlayCardToZone(Card, PFRange, 2, "Shurima");
         PlayCardToZone(Card, PFSiege, 3, "Shurima");
@@ -64,19 +66,22 @@ public class AltPlayCards : MonoBehaviour
 
         PlayDecoyCard(Card, EFMelee, 13);
         PlayDecoyCard(Card, EFRange, 14);*/
-    }
+        }
 }
 
 private void PlayCardToZone(GameObject card, GameObject zone, int numericLine, string faction)
 {
-    if (card.GetComponent<CardClass>().Faction == faction && card.GetComponent<CardClass>().NumericLine == numericLine && Turn && PlayerSurrender == false /*&& PlayerChooses*/)
+    if (card.transform.parent != zone.transform)
     {
-        if (playable)
+        if (card.GetComponent<CardClass>().Faction == faction && card.GetComponent<CardClass>().NumericLine == numericLine /*&& Turn*/ && PlayerSurrender == false /*&& PlayerChooses*/)
         {
+            if (playable)
+            {
             card.transform.SetParent(zone.transform, false);
             card.transform.position = zone.transform.position;
             playable = false;
             // sound.Play();
+            }
         }
     }
 }
